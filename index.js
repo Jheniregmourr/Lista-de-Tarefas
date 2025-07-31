b756ad5701eb77e64eb5d3a4e938272e0b963fe6
Jhenifer <jhenifereginademoura@gmail.com>
2025-07-31 15:15:55 -0300

    feat: adicionando novas funcionalidades X e Y.
    Adicionadas duas novas funionalidade:
    X = Edição de tarefa.
    Y = Exclusão de tarefa.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var listaTarefas = document.getElementById("lista-tarefas");
 var btnAdc = document.getElementById("btn-salvarItem");
 var inputItem = document.getElementById("input-item");
+var contador = 0;
 
 btnAdc.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -9,19 +10,62 @@ btnAdc.addEventListener("click", (evento) => {
         alert("Insira uma tarefa para continuar.");
         return
     }
-
+    var organizarDisposicao = document.createElement("div")
+    organizarDisposicao.classList.add("organizar-itens")
     var lista = document.createElement("li");
     var itemLista = document.createElement("div");
-    itemLista.classList.add = ("item-lista");
+    itemLista.classList.add("item-lista");
     var checkBox = document.createElement("input");
     checkBox.type = "checkbox";
     var textItem = document.createElement("p");
+    //itens do icons
+    var conteinerIcons = document.createElement("div");
+    conteinerIcons.classList.add("conteiner-icons");
 
-    inputItem.value = inputItem.value.charAt(0).toUpperCase()+inputItem.value.slice(1);
+    contador = contador+1;
+
+    var btnEditar = document.createElement("button");
+    btnEditar.id = `btn-edicao-${contador}`;
+    var imgEdicao = document.createElement("img");
+
+    imgEdicao.src = "assets/img/lápis.svg";
+    imgEdicao.title = "Editar";
+
+    var btnRemover = document.createElement("button");
+    btnRemover.id = `btn-remover-${contador}`;
+    var imgRemover = document.createElement("img");
+
+    imgRemover.src = "assets/img/lixeira.svg";
+    imgRemover.title = "Remover";
+
+    btnEditar.addEventListener("click", (event) => {
+
+    var texto_editar = prompt("Novo item: ")
+
+    textItem.innerText = texto_editar;
+
+    })
+
+    btnRemover.addEventListener("click", (event) => {
+
+        listaTarefas.removeChild(organizarDisposicao);
+        Verificar_lista_vazia()
+
+    })
+
+    //final dos itens dos icons
+
+    inputItem.value = inputItem.value.charAt(0).toUpperCase()+inputItem.value.slice(1); 
 
     textItem.innerText = inputItem.value;
 
-    listaTarefas.appendChild(lista);
+    listaTarefas.appendChild(organizarDisposicao);
+    organizarDisposicao.appendChild(lista);
+    organizarDisposicao.appendChild(conteinerIcons)
+    conteinerIcons.appendChild(btnEditar)
+    btnEditar.appendChild(imgEdicao)
+    conteinerIcons.appendChild(btnRemover)
+    btnRemover.appendChild(imgRemover)
     lista.appendChild(itemLista);
     itemLista.appendChild(checkBox);
     itemLista.appendChild(textItem);
@@ -40,7 +84,7 @@ btnAdc.addEventListener("click", (evento) => {
     var dataHora = `${data} (${dataEspecifica}) às ${hora}.`; //DATA E HORA ARRUMARRRR!!!!!!!
 
     var dataHoraLocal = document.createElement("p");
-    dataHoraLocal.classList.add = ("datas");
+    dataHoraLocal.classList.add("datas");
     dataHoraLocal.textContent = dataHora;
     lista.appendChild(dataHoraLocal);
 
@@ -56,6 +100,12 @@ btnAdc.addEventListener("click", (evento) => {
 
     Verificar_lista_vazia();
 
+btnEdicao.addEventListener("click", (evento) => {
+    evento.preventDefault()
+
+    //Editar os itens de inputItem
+})
+
 })
 
 function Verificar_lista_vazia(){
@@ -70,3 +120,10 @@ function Verificar_lista_vazia(){
 }
 
 Verificar_lista_vazia();
+
+    document.getElementById("btn-remover").addEventListener("click", (event) => {
+
+        listaTarefas.removeChild(organizarDisposicao);
+        Verificar_lista_vazia()
+    
+    })
